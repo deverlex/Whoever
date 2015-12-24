52f28e73424ea14cc3f3034d27222f3bfe8dc702
Spider man <nguyendo94vn@gmail.com>
2015-12-24 18:32:44 +0700

    remove idea login with facebook to system account

--- Whoever/app/src/main/java/vn/whoever/StartActivity.java
+++ Whoever/app/src/main/java/vn/whoever/StartActivity.java
@@ -1,29 +1,38 @@
 package vn.whoever;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.service.textservice.SpellCheckerService.Session;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -40,6 +49,7 @@ public class StartActivity extends AppCompatActivity {
 
     private LoginButton loginButtonFaceBook;
     private CallbackManager callbackManagerFacebook;
+    private ProfileTracker profileTrackerFaceBook;
 
 
     @Override
@@ -64,16 +74,82 @@ public class StartActivity extends AppCompatActivity {
         Typeface bauhau93_font = Typeface.createFromAsset(getAssets(), "fonts/bauhau93.ttf");
         logoText.setTypeface(bauhau93_font);
 
-        setLoginFaceBook();
+        callbackManagerFacebook = CallbackManager.Factory.create();
 
+        loginButtonFaceBook = (LoginButton) findViewById(R.id.loginFaceBookButton);
+
+        loginButtonFaceBook.setReadPermissions(Arrays.asList("email, public_profile"));
+        loginButtonFaceBook.registerCallback(callbackManagerFacebook, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject me, GraphResponse response) {
+
+
+                                if (response.getError() != null) {
+
+                                } else {
+                                    String email = "";
+                                    try {
+                                        email = response.getJSONObject().getJSONObject("email").getJSONObject("data").getString("email");
+                                        Log.e("Result Email: ", email);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    email = me.optString("email");
+                                    String id = me.optString("id");
+                                    String name = me.optString("name");
+
+                                    Log.d("NAME: ", name);
+                                    Log.d("ID: ", id);
+                                    Log.d("EMAIL: ", email);
+                                }
+                            }
+                        }).executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        /*
+        loginButtonFaceBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLoginFaceBook();
+            }
+        });
+        */
+    }
+
+    public void getProfileTracker() {
+        profileTrackerFaceBook = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile,
+                                                   Profile currentProfile) {
+                String fistName = currentProfile.getName();
+                Log.d("Name of user: ", fistName);
+            }
+        };
     }
 
     public void setLoginFaceBook() {
         callbackManagerFacebook = CallbackManager.Factory.create();
 
-        List<String> permissionNeeds= Arrays.asList("user_photos", "friends_photos", "email", "user_birthday", "user_friends");
+        //List<String> permissionNeeds= Arrays.asList("email", "user_birthday", "user_friends");
 
-        LoginManager.getInstance().logInWithReadPermissions(this, permissionNeeds);
+        //LoginManager.getInstance().logInWithReadPermissions(this, permissionNeeds);
         LoginManager.getInstance().registerCallback(callbackManagerFacebook, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -85,13 +161,17 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onCancel() {
                 // not called
-                Log.d("fb_login_sdk", "callback cancel");
+                Toast toastLoginCancel = Toast.makeText(getApplicationContext(),
+                        getString(R.string.login_cancel), Toast.LENGTH_SHORT);
+                toastLoginCancel.show();
             }
 
             @Override
             public void onError(FacebookException e) {
                 // not called
-                Log.d("fb_login_sdk", "callback onError");
+                Toast toastLoginError = Toast.makeText(getApplicationContext(),
+                        getString(R.string.login_error), Toast.LENGTH_SHORT);
+                toastLoginError.show();
             }
         });
     }
@@ -106,20 +186,24 @@ public class StartActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManagerFacebook.onActivityResult(requestCode, resultCode, data);
+    //    getProfileTracker();
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-
         AppEventsLogger.activateApp(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ Whoever/app/src/main/java/vn/whoever/utils/CharArray.java
@@ -0,0 +1,8 @@
+package vn.whoever.utils;
+
+/**
+ * Created by spider man on 12/24/2015.
+ */
+public class CharArray {
+
+}
