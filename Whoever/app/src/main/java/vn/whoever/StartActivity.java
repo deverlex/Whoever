e86f7b3d689fa617f73d5bb67a3d645e7fe9c575
Spider man <nguyendo94vn@gmail.com>
2015-12-24 14:43:10 +0700

    call back facebook result data

--- Whoever/app/src/main/java/vn/whoever/StartActivity.java
+++ Whoever/app/src/main/java/vn/whoever/StartActivity.java
@@ -3,14 +3,31 @@ package vn.whoever;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by spider man on 12/22/2015.
@@ -21,6 +38,10 @@ public class StartActivity extends AppCompatActivity {
     public static final String PREFERENCES = "PREFERENCES_LOCAL";
     public static SharedPreferences sharedPreferences;
 
+    private LoginButton loginButtonFaceBook;
+    private CallbackManager callbackManagerFacebook;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +49,6 @@ public class StartActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.start_layout);
 
         sharedPreferences = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
         boolean isLogin = sharedPreferences.getBoolean("isLogin", false);
@@ -38,10 +58,56 @@ public class StartActivity extends AppCompatActivity {
             finish();
         }
 
+        setContentView(R.layout.start_layout);
+
         TextView logoText = (TextView) findViewById(R.id.logoTextStart);
-        Typeface bauhau93_font = Typeface.createFromAsset(getAssets(),  "fonts/bauhau93.ttf");
+        Typeface bauhau93_font = Typeface.createFromAsset(getAssets(), "fonts/bauhau93.ttf");
         logoText.setTypeface(bauhau93_font);
+
+        setLoginFaceBook();
+
     }
+
+    public void setLoginFaceBook() {
+        callbackManagerFacebook = CallbackManager.Factory.create();
+
+        List<String> permissionNeeds= Arrays.asList("user_photos", "friends_photos", "email", "user_birthday", "user_friends");
+
+        LoginManager.getInstance().logInWithReadPermissions(this, permissionNeeds);
+        LoginManager.getInstance().registerCallback(callbackManagerFacebook, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // not called
+                Log.d("fb_login_sdk", "callback success");
+                navigateToMain();
+            }
+
+            @Override
+            public void onCancel() {
+                // not called
+                Log.d("fb_login_sdk", "callback cancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                // not called
+                Log.d("fb_login_sdk", "callback onError");
+            }
+        });
+    }
+
+    public void navigateToMain() {
+        Intent intentMain = new Intent(this, MainActivity.class);
+        startActivity(intentMain);
+        finish();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManagerFacebook.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     protected void onResume() {
