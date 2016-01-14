692101be97b4f8e19737309e9c3713d704e9fdff
Spider man <nguyendo94vn@gmail.com>
2016-01-14 22:28:17 +0700

    start design post status

---
+++ Whoever/app/src/main/java/vn/whoever/activities/PostStatusActivity.java
@@ -0,0 +1,28 @@
+package vn.whoever.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import vn.whoever.R;
+
+/**
+ * Created by spider man on 1/14/2016.
+ */
+public class PostStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.write_status_layout);
+
+
+    }
+
+
+
+}
