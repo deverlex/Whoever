4581f250d152d666dc3008f62a362d30e255c6b3
Spider man <nguyendo94vn@gmail.com>
2016-01-17 15:48:15 +0700

    end day 17/1/2016

--- Whoever/app/src/main/java/vn/whoever/customviews/JTextView.java
+++ Whoever/app/src/main/java/vn/whoever/customviews/JTextView.java
@@ -1,6 +1,9 @@
 package vn.whoever.customviews;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Typeface;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,9 +15,129 @@ import java.util.ArrayList;
 public class JTextView extends View {
 
     private String text;
+    private ArrayList<Line> lines;
+    private TextPaint textPaint;
+    private Typeface font;
+    private int textColor;
 
-    public JTextView(Context context, AttributeSet attr) {
+    private float textSize;
+    private float lineHeight;
+    private float wordSpace;
+    private float lineSpace;
+
+    private float onBirim;
+    private float width;
+    private float height;
+    private float leftPadding;
+    private float rightPadding;
+
+    public JTextView(Context context, String text) {
         super(context);
+        lines = new ArrayList<Line>();
+        this.text = text;
+    }
+
+    public void init() {
+
+    }
+
+    @Override
+    protected void onMeasure(int wMeasureSpace, int hMeasureSpace) {
+        super.onMeasure(wMeasureSpace, hMeasureSpace);
+        
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ArrayList<Line> getLines() {
+        return lines;
+    }
+
+    public void setLines(ArrayList<Line> lines) {
+        this.lines = lines;
+    }
+
+    public TextPaint getTextPaint() {
+        return textPaint;
+    }
+
+    public void setTextPaint(TextPaint textPaint) {
+        this.textPaint = textPaint;
+    }
+
+    public Typeface getFont() {
+        return font;
+    }
+
+    public void setFont(Typeface font) {
+        this.font = font;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+    }
+
+    public float getWordSpace() {
+        return wordSpace;
+    }
+
+    public void setWordSpace(float wordSpace) {
+        this.wordSpace = wordSpace;
+    }
+
+    public float getLineHeight() {
+        return lineHeight;
+    }
+
+    public void setLineHeight(float lineHeight) {
+        this.lineHeight = lineHeight;
+    }
+
+    public float getLineSpace() {
+        return lineSpace;
+    }
+
+    public void setLineSpace(float lineSpace) {
+        this.lineSpace = lineSpace;
+    }
+
+    public float getLeftPadding() {
+        return leftPadding;
+    }
+
+    public void setLeftPadding(float leftPadding) {
+        this.leftPadding = leftPadding;
+    }
+
+    public float getRightPadding() {
+        return rightPadding;
+    }
+
+    public void setRightPadding(float rightPadding) {
+        this.rightPadding = rightPadding;
     }
 
     class Line {
@@ -51,5 +174,4 @@ public class JTextView extends View {
             this.words.add(str);
         }
     }
-
 }
