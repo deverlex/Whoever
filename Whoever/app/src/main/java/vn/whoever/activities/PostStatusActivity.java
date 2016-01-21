27ffcb3a2a2555d327480f1a822635dfb6da90a3
Spider man <nguyendo94vn@gmail.com>
2016-01-21 11:02:38 +0700

    start design post status layout

--- Whoever/app/src/main/java/vn/whoever/activities/PostStatusActivity.java
+++ Whoever/app/src/main/java/vn/whoever/activities/PostStatusActivity.java
@@ -2,7 +2,9 @@ package vn.whoever.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import vn.whoever.R;
 import vn.whoever.utils.AppGc;
@@ -11,6 +13,10 @@ import vn.whoever.utils.AppGc;
  * Created by spider man on 1/14/2016.
  */
 public class PostStatusActivity extends AppCompatActivity implements AppGc {
+
+    private RelativeLayout toolbar;
+    private RelativeLayout btnBack;
+    private RelativeLayout btnPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,32 @@ public class PostStatusActivity extends AppCompatActivity implements AppGc {
 
         setContentView(R.layout.post_status_layout);
 
+        init();
+        initListener();
+    }
+
+    public void init() {
+        toolbar = (RelativeLayout) findViewById(R.id.toolBarFromPostStatus);
+        btnBack = (RelativeLayout) toolbar.findViewById(R.id.btnBackFromPostStatus);
+        btnPost = (RelativeLayout) toolbar.findViewById(R.id.btnSendStatus);
+
+    }
+
+    public void initListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                finish();
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
